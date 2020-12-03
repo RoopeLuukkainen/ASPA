369dda1ad9f8912a9a55ddc9dde7b2e5f59e38c5
Luke <roope.luukkainen@gmail.com>
2020-12-03 09:21:38 +0200

    Created directory structure for message testfiles

---
+++ tests/PT_basic_command/PT1_print_call.py
@@ -0,0 +1,9 @@
+"""Test file for PT1 check.
+PT1 check detects usage of function calls defined in configuration.
+"""
+
+def PT1(): 
+    print("Hello World!")
+    temp = range(len(str(int(round(float("1.2345"), 2)))))
+    return None
+# PT1()
