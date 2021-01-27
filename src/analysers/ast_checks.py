f3389dc1fff726c967a46f44efe9b03114d82104
Luke <roope.luukkainen@gmail.com>
2021-01-27 16:58:23 +0200

    Moved ast checks from visits to _private funcs.

--- src/analysers/ast_checks.py
+++ src/analysers/ast_checks.py
@@ -1,14 +1,1 @@
 """Library file containing generally used ASPA ast checks."""
-
-import ast
-
-import src.analysers.analysis_utils as au
-import src.config.config as cnf
-
-# AST checks
-def has_exception_handling(node, denied=cnf.FUNC):
-    """ Check to determine if node is inside exception handling."""
-
-    if au.get_parent(node, ast.Try, denied=denied) is None:
-       return False
-    return True
