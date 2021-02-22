47c24c621e0ff7530b915b035eadebaa12929f97
Luke <roope.luukkainen@gmail.com>
2021-02-22 16:21:39 +0200

    Improved test files of TR2-1 and TR2-2 checks.

--- tests/TR_data_structure/TR2-1_using_class_without_object.py
+++ tests/TR_data_structure/TR2-1_using_class_without_object.py
@@ -2,6 +2,8 @@
 TR2-1 check detects direct usage of class attributes without an object.
 """
 
+import example_lib_TR
+
 class FOO():
     attr = 1
 
@@ -9,6 +11,9 @@ class FOO():
 def TR2_1():
     FOO.attr = 2
     print(FOO.attr)
+
+    # FIXME check for class usage from different files
+    example_lib_TR.BAR.attribute = 2
 
     return None
 # TR2_1()
--- tests/TR_data_structure/TR2-2_object_creation_miss_parenthesis.py
+++ tests/TR_data_structure/TR2-2_object_creation_miss_parenthesis.py
@@ -3,6 +3,8 @@ TR2-2 check detects object creation which misses parenthesis, i.e.
 object = CLASS, instead of object = CLASS().
 """
 
+import example_lib_TR
+
 class FOO():
     attr = 1
 
@@ -13,5 +15,12 @@ def TR2_2(thing):
     print(bar.attr)
 
     thing.a.b.c.d = FOO
+
+    foo = FOO()
+    foo.attr = 3
+
+    # FIXME check for class usage from different files
+    obj = example_lib_TR.BAR
+
     return None
 # TR2_2()
